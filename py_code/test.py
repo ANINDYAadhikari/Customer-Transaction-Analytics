b070bc081aae176ad8fab378bfd324bd8c16ffdd
ANINDYAadhikari <anindyaadhikari140@gmail.com>
2026-04-22 09:46:34 +0530

    Improvements

---
+++ py_code/test.py
@@ -0,0 +1,13 @@
+# ======================================
+# Customer-Transaction-Analytics PROJECT
+# --------------------------------------
+# Author -- Anindya Adhikari
+# ======================================
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import create_engine
+
+plt.style.use('seaborn-v0_8')
